3fb4fb799d1bc1b3cb55ad72fc8be9bb60ca11cf
Chaitanya Chalasani <cchalasani@me.com>
2016-07-10 16:05:48 +0530

    I have created a new skeleton for the voyage product

---
+++ voyage/apps/voyage/include/voyage.hrl
@@ -0,0 +1,11 @@
+-record(users, {
+          id,
+          name,
+          phoneno,
+          address,
+          pin,
+          email,
+          password,
+          created_on,
+          modifed_on
+         }).
